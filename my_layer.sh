dbee5a6aacb25a968f640e2f6821cfa1787bf039
Chemsdev <chemscreative@gmail.com>
2025-06-13 11:57:02 +0200

    commit

---
+++ my_layer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "🔄 Nettoyage..."
+rm -rf python terraform/layer.zip
+
+echo "📦 Installation des dépendances..."
+mkdir -p python
+pip install -r lambda_function/requirements.txt -t python/
+
+echo "📁 Zippage en layer.zip..."
+cd python
+zip -r ../terraform/layer.zip .
+cd ..
+
+rm -rf python
+
+echo "✅ Layer prêt : terraform/layer.zip"
