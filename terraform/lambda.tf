5a1c117f6c1667fa1f3bb102cd775787957981de
Chemsdev <chemscreative@gmail.com>
2025-06-13 12:59:47 +0200

    test

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,13 +2,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-
 resource "aws_lambda_function" "my_lambda" {
   function_name = "my-lambda-function"
   runtime       = "python3.9"
-  handler       = "main.lambda_handler"
+  handler       = "app.lambda_handler" # ou main.lambda_handler selon ton fichier .py
+
   filename         = "${path.module}/function.zip"
   source_code_hash = filebase64sha256("${path.module}/function.zip")
-  role = "arn:aws:iam::115083608235:role/lambda-s3-role"  
+
+  role = "arn:aws:iam::115083608235:role/lambda-s3-role" 
+
   timeout = 10
 }
