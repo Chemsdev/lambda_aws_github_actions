f2586094afef20552464fc768067defd2a843ec8
Chemsdev <chemscreative@gmail.com>
2025-06-13 12:56:15 +0200

    test

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -7,15 +7,8 @@ resource "aws_lambda_function" "my_lambda" {
   function_name = "my-lambda-function"
   runtime       = "python3.9"
   handler       = "main.lambda_handler"
-
   filename         = "${path.module}/function.zip"
   source_code_hash = filebase64sha256("${path.module}/function.zip")
-
   role = "arn:aws:iam::115083608235:role/lambda-s3-role"  
-
-  layers = [
-    aws_lambda_layer_version.python_dependencies.arn
-  ]
-
   timeout = 10
 }
--- terraform/layer.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lambda_layer_version" "python_dependencies" {
-  filename             = "${path.module}/layer.zip"
-  layer_name           = "my-python-layer-test"
-  compatible_runtimes  = ["python3.9"]
-  source_code_hash     = filebase64sha256("${path.module}/layer.zip")
-}
-
